328819828dfc0e5d64da2361de87f71805584339
guzhenhua <7070@qq.com>
2020-01-19 17:52:51 +0800

    '创建模板之后接受客户端参数'

--- testplafrom/personal/views.py
+++ testplafrom/personal/views.py
@@ -3,4 +3,4 @@ from  django.http import HttpResponse
 # Create your views here.
 def say_hello(request):
     name=request.GET.get("name","")
-    return  HttpResponse("hello world,"+name)
+    return  render(request,"index.html",{"name":name})
